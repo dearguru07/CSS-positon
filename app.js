772e729594c63155b122029b7485755be4cd53f9
dearguru07 <pangalaguruyadav101@gmail.com>
2024-09-23 09:35:42 +0530

    'a'

---
+++ app.js
@@ -0,0 +1,75 @@
+// const Object = {
+//   ProductName: "Iphone 16",
+//   Price: 19999,
+//   Color: "Pure White",
+//   Rating: 4.6,
+//   Avalible: true,
+//   Specifications: {
+//     Camera: 48 + "MP",
+//     Storage: 256 + "GB",
+//     Ram: 8 + "GB",
+//     Battery: 4850 + "mAh",
+//     Display: "oLED",
+//   },
+//   ProductName: "Samsung S23",
+//   Price: 99999,
+//   Color: "Pure Black",
+//   Rating: 4.1,
+//   Avalible: true,
+//   Specifications: {
+//     Camera: 108 + "MP",
+//     Storage: 512 + "GB",
+//     Ram: 12 + "GB",
+//     Battery: 5010 + "mAh",
+//     Display: "oLED",
+//   },
+//   ProductName: "Mi",
+//   Price: 19999,
+//   Color: "Pure White",
+//   Rating: 4.6,
+//   Avalible: true,
+//   Specifications: {
+//     Camera: 48 + "MP",
+//     Storage: 256 + "GB",
+//     Ram: 8 + "GB",
+//     Battery: 4850 + "mAh",
+//     Display: "oLED",
+//   }
+// };
+// console.log(Object)
+
+
+
+
+
+
+// -------------------Reduces & Finding the maxx value----------------------
+
+// let numbers=[2,5,1,6,45,78,32,-1]
+
+// let results=numbers.reduce((max,cur)=>{
+//    if(cur>max){
+//     max=cur
+//    }
+//    return max
+// },0)
+// console.log(results)
+
+// ----------------------------Adding the numbersusing Reduces------------------------------
+
+// let numbers = [2, 5, 8, 9, 6, 55, 45, 75, 2];
+
+// let total = numbers.reduce((tot, curr) => {
+//   res = tot + curr;
+//   return res
+// });
+// console.log(total)
+
+
+// let numbers = [2, 5, 8, 9, 6, 55, 45, 75, 2];
+
+// let result=numbers.map(function(x){
+//     return x*x
+// })
+// console.log(result)
+
